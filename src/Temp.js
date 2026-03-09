2fb4f549e300b1bbb6bacf8ceaf90825a40ce696
Emily01749 <etmayer@ucsc.edu>
2026-03-09 11:59:52 -0700

    Added playerMovement & camera functions + made tilemap bigger

--- src/Temp.js
+++ src/Temp.js
@@ -4,6 +4,7 @@ export class Tmp extends Phaser.Scene {
         super('Tmp');
     }
 
+    // Moves the player around the map
     playerMovement(speed){
 
         this.playerKeys = this.input.keyboard.addKeys({up: "W", down: "S", left: "A", right: "D"});
@@ -26,8 +27,15 @@ export class Tmp extends Phaser.Scene {
         }
     }
 
+    // Camera follows the player around
+    camera(map, player){
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(player, true, 0.1, 0.1);
+    }
+
     preload() {
 
+        // Load the Cat from the Spritesheet
         this.load.spritesheet("retro-cats-sheet", "assets/RetroCatsFree.png", {
             frameWidth: 64, 
             frameHeight: 64, 
@@ -35,29 +43,34 @@ export class Tmp extends Phaser.Scene {
             startFrame: 0,
             endFrame: 0
         });
+
+        // Floors and Walls
         this.load.image("floors-walls-sheet", "assets/TopDownHouse_FloorsAndWalls.png");
+
+        // Level Tilemap (Subject to Change)
         this.load.tilemapTiledJSON('temporary-tilemap', 'assets/Map-tmp.tmj');
     }
 
     create() {
 
         //Adding tile map
-        const tempMap = this.make.tilemap({key: 'temporary-tilemap'});
+        this.tempMap = this.make.tilemap({key: 'temporary-tilemap'});
 
-        const floorWalls = tempMap.addTilesetImage('TopDownHouse_FloorsAndWalls', 'floors-walls-sheet');
+        this.floorWalls = this.tempMap.addTilesetImage('TopDownHouse_FloorsAndWalls', 'floors-walls-sheet');
 
-        const floor = tempMap.createLayer('Tile Layer 1', floorWalls);
+        this.floor = this.tempMap.createLayer('Tile Layer 1', this.floorWalls);
 
 
         //Adding player
 
         // Temporary as a cat (change to mouse later)
         this.player = this.physics.add.sprite(350,600,"retro-cats-sheet", 0);
-        this.player.setCollideWorldBounds(true);
+        // this.player.setCollideWorldBounds(true);
     }
 
     update() {
         this.playerMovement(200);
+        this.camera(this.tempMap, this.player);
     }
     
 }
